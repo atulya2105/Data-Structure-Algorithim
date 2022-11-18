a6459f108fba12b6edeae6efc14a0910c7f55a64
Arvind Pandey <84077760+atulya2105@users.noreply.github.com>
2022-11-18 23:31:45 +0530

    Time: 0 ms (100.00%), Space: 42.6 MB (36.27%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.java
+++ 0075-sort-colors/0075-sort-colors.java
@@ -3,21 +3,20 @@ class Solution {
         int i = 0;
         int j = 0;
         int k = arr.length-1;
-        while(j<=k){
-            if(arr[j]==0){
-                int temp = arr[j];
-                arr[j] = arr[i];
-                arr[i] = temp;
+        while(i<=k){
+            if(arr[i]==0){
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
                 i++;
                 j++;
-            }else if(arr[j]==1){
-                j++;
-            }else if(arr[j]==2){
-                int temp = arr[j];
-                arr[j] = arr[k];
+            }else if(arr[i]==1){
+                i++;
+            }else if(arr[i]==2){
+                int temp = arr[i];
+                arr[i] = arr[k];
                 arr[k] = temp;
                 k--;
-                
             }
         }
     }
