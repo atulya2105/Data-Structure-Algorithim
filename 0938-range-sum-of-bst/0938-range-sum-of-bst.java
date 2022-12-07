f4c08b2c798f66eb16a8b39cf164de27c45f72e6
Arvind Pandey <84077760+atulya2105@users.noreply.github.com>
2022-12-07 11:04:12 -0800

    Time: 0 ms (100.00%), Space: 67.1 MB (44.58%) - LeetHub

--- 0938-range-sum-of-bst/0938-range-sum-of-bst.java
+++ 0938-range-sum-of-bst/0938-range-sum-of-bst.java
@@ -17,11 +17,17 @@ class Solution {
     int sum = 0;
     public int rangeSumBST(TreeNode root, int low, int high) {
         if(root== null) return 0;
-        if(root.val>= low && root.val<=high){
+        if(root.val<low && root.val<high){
+            rangeSumBST(root.right,low,high);
+        }else if(root.val>high && root.val>high){
+            rangeSumBST(root.left,low,high);
+        }else{
             sum += root.val;
+            rangeSumBST(root.left,low,high);
+            rangeSumBST(root.right,low,high);
         }
-        rangeSumBST(root.left,low,high);
-        rangeSumBST(root.right,low,high);
+        
+        
         return sum;
     }
 }
