6a062b020c3becca701b032ccfa06b4af77312de
Arvind Pandey <84077760+atulya2105@users.noreply.github.com>
2023-01-31 03:42:47 -0800

    Time: 24 ms (81.47%), Space: 13.9 MB (87.28%) - LeetHub

---
+++ 1903-largest-odd-number-in-string/1903-largest-odd-number-in-string.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    string largestOddNumber(string num) {
+        for(int i=num.size()-1;i>=0;i--){
+            if(num[i]%2!=0)
+                break;
+            else{
+                num.pop_back();
+            }
+        }
+        return num;
+    }
+};
