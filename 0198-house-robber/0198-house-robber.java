80b5e0d0db4995194fc557c5bb9f1f8b11884aef
Arvind Pandey <84077760+atulya2105@users.noreply.github.com>
2023-02-18 13:07:09 -0800

    Time: 0 ms (100.00%), Space: 39.6 MB (67.58%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -1,12 +1,26 @@
 class Solution {
     public int rob(int[] nums) {
-        int prevNo = 0;
-    int prevYes = 0;
-    for (int n : nums) {
-        int temp = prevNo;
-        prevNo = Math.max(prevNo, prevYes);
-        prevYes = n + temp;
+    //     int prevNo = 0;
+    // int prevYes = 0;
+    // for (int n : nums) {
+    //     int temp = prevNo;
+    //     prevNo = Math.max(prevNo, prevYes);
+    //     prevYes = n + temp;
+    // }
+    // return Math.max(prevNo, prevYes);
+        // Memorization method
+        int[] dp = new int[nums.length];
+        Arrays.fill(dp,-1);
+        return helper(nums,nums.length-1,dp);
     }
-    return Math.max(prevNo, prevYes);
+    int helper(int[] nums, int idx,int[] dp){
+        if(idx==0) return nums[idx];
+        if(idx<0) return 0;
+        if(dp[idx]!=-1)
+            return dp[idx];
+        int pick = nums[idx] + helper(nums,idx-2,dp);
+        int notPick = helper(nums,idx-1,dp);
+        dp[idx] = Math.max(pick,notPick);
+        return dp[idx];
     }
 }
