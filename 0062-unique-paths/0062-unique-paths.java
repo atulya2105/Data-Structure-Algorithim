93be5de17a88ecdf8b79a034b85eeb17d5cbc846
Arvind Pandey <84077760+atulya2105@users.noreply.github.com>
2023-02-21 12:50:05 -0800

    Time: 0 ms (100.00%), Space: 40.9 MB (11.43%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.java
+++ 0062-unique-paths/0062-unique-paths.java
@@ -13,26 +13,45 @@ class Solution {
         return dp[m][n] = up + left;
     }
     public int uniquePaths(int m, int n) {
-        int[][] dp = new int[m][n];
+        // int[][] dp = new int[m][n];
         // return uniquePath(m-1,n-1,dp);
         
         // tabulation method to solve this question 
         
+        // for(int i = 0;i<m;i++){
+        //     for(int j = 0;j<n;j++){
+        //         int up = 0;
+        //         int left  = 0;
+        //         if(i==0 && j == 0){
+        //             dp[i][j] = 1;
+        //         }else{
+        //             if(i>0) 
+        //                 up = dp[i-1][j];
+        //             if(j>0)
+        //                 left = dp[i][j-1];
+        //             dp[i][j] = up+left;
+        //         }
+        //     }
+        // }
+        
+        int[] prev = new int[n];
         for(int i = 0;i<m;i++){
+            int[] curr = new int[n];
             for(int j = 0;j<n;j++){
                 int up = 0;
                 int left  = 0;
                 if(i==0 && j == 0){
-                    dp[i][j] = 1;
+                    curr[j] = 1;
                 }else{
                     if(i>0) 
-                        up = dp[i-1][j];
+                        up = prev[j];
                     if(j>0)
-                        left = dp[i][j-1];
-                    dp[i][j] = up+left;
+                        left = curr[j-1];
+                    curr[j] = up+left;
                 }
             }
+            prev = curr;
         }
-        return dp[m-1][n-1];
+        return prev[n-1];
     }
 }
