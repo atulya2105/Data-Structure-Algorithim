482613e09a3d7f818372b1a11f971a745b732afb
Arvind Pandey <84077760+atulya2105@users.noreply.github.com>
2023-02-21 13:09:23 -0800

    Time: 1 ms (23.94%), Space: 42.4 MB (7.83%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.java
+++ 0063-unique-paths-ii/0063-unique-paths-ii.java
@@ -18,6 +18,23 @@ class Solution {
         int m = obstacleGrid[0].length;
         int[][] dp = new int[n][m];
         
-        return memo(obstacleGrid,n-1,m-1,dp);
+        for(int i = 0;i<n;i++){
+            for(int j = 0;j<m;j++){
+                int up = 0;
+                int left = 0;
+                if(i>= 0 && j>=0 && obstacleGrid[i][j]==1)
+                    dp[i][j] =  0;
+                else if(i==0 && j==0)
+                    dp[i][j] = 1;
+                else{
+                    if(i>0)
+                        up = dp[i-1][j];
+                    if(j>0)
+                        left = dp[i][j-1];
+                    dp[i][j] = up+left;
+                }
+            }
+        }
+        return dp[n-1][m-1];
     }
 }
