8fb254b81db438044dd6ef844b7d7dfab7591db5
Arvind Pandey <84077760+atulya2105@users.noreply.github.com>
2023-02-22 03:05:16 -0800

    Time: 4 ms (64.44%), Space: 47.8 MB (5.67%) - LeetHub

--- 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.java
+++ 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.java
@@ -1,37 +1,50 @@
 class Solution {
-    public int minFallingPathSum(int[][] A) {
-        if(A == null || A.length == 0 || A[0].length == 0) return 0;
+    int memo(int[][] arr,int i,int j,int[][] dp){
         
-        int m = A.length;
-        int n = A[0].length;
-        int[][] M = new int[m][n]; // M[i][j] represents the min sum from top to A[i][j]
-        // M[0][j] stays the same
-        // M[i][j] = min(M[i - 1][j - 1], M[i - 1][j], M[i - 1][j + 1]) + A[i][j]
-        
-        // copy the 1st row to M[0]
-        for(int j = 0; j < n; j++){
-            M[0][j] = A[0][j];    
+         if(j<0 || j>=arr[0].length){
+            return Integer.MAX_VALUE;
         }
+        if(i==0)
+            return arr[i][j];
+       if(dp[i][j] != 0){
+           return dp[i][j];
+       }
         
-        for(int i = 1; i < m; i++){
-            for(int j = 0; j < n; j++){
-                if(j == 0){
-                    M[i][j] = Math.min(M[i - 1][j], M[i - 1][j + 1]);
-                }else if(j == n - 1){
-                    M[i][j] = Math.min(M[i - 1][j - 1], M[i - 1][j]);
-                }else{
-                    M[i][j] = Math.min(M[i - 1][j - 1], M[i - 1][j]);
-                    M[i][j] = Math.min(M[i][j], M[i - 1][j + 1]);
+        int up = arr[i][j] + memo(arr,i-1,j,dp);
+        int leftD = arr[i][j] + memo(arr,i-1,j-1,dp);
+        int rightD = arr[i][j] + memo(arr,i-1,j+1,dp);
+        
+        return dp[i][j] = Math.min(leftD,Math.min(up,rightD));
+    }
+    public int minFallingPathSum(int[][] A) {
+        int dp[][] = new int[A.length][A.length];
+        int min = Integer.MAX_VALUE;
+        for(int i = 0; i < A.length; i++)
+        {
+            dp[0][i] = A[0][i];
+        }
+        for(int i = 1; i < A.length; i++)
+        {
+            for(int j = 0; j < A.length; j++)
+            {
+                if(j == 0)
+                {
+                    dp[i][j] = A[i][j] + Math.min(dp[i-1][j], dp[i-1][j+1]);
                 }
-                M[i][j] += A[i][j];
+                else if(j == A.length - 1)
+                {
+                    dp[i][j] = A[i][j] + Math.min(dp[i-1][j-1], dp[i-1][j]);
+                }
+                else
+                {
+                    dp[i][j] = A[i][j] + Math.min(Math.min(dp[i-1][j-1], dp[i-1][j]), dp[i-1][j+1]);
+                }
             }
         }
-        
-        int min = Integer.MAX_VALUE;
-        for(int num : M[m - 1]){
-            min = Math.min(min, num);
+        for(int i = 0; i < A.length; i++)
+        {
+            if(dp[A.length-1][i] < min)  min = dp[A.length-1][i];
         }
-        
-        return min; 
+        return min;
     }
 }
